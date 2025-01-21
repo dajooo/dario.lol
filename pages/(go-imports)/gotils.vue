34682e6812c801a00bd936299eadddb663a01c79
dajooo <contact@dajooo.de>
2025-01-21 17:57:13 +0100

    fix: go import repo

---
+++ pages/(go-imports)/gotils.vue
@@ -0,0 +1,8 @@
+<template>
+  <Head>
+    <meta name="go-import" content="dario.lol/gotils git https://github.com/dajooo/gotils">
+    <meta name="go-source" content="dario.lol/gotils _ https://github.com/dajooo/gotils/tree/main{/dir} https://github.com/dajooo/gotils/blob/main{/dir}/{file}#L{line}">
+    <meta http-equiv="refresh" content="0; url=https://pkg.go.dev/dario.lol/gotils">
+    <title>dario.lol/gotils</title>
+  </Head>
+</template>
