0f10e886aad14810c87a67afd9346e65602722f9
dajooo <contact@dajooo.de>
2025-01-22 18:16:25 +0100

    fix: add go-source meta tag for vanity url

--- server/routes/gotils.ts
+++ server/routes/gotils.ts
@@ -1,7 +1,12 @@
 export default defineEventHandler((event) => {
   if (getQuery(event)['go-get'] === '1') {
     setResponseHeader(event, 'Content-Type', 'text/html')
-    return `<html><head><meta name="go-import" content="dario.lol/gotils git https://github.com/dajooo/gotils" /></head></html>`
+    return `<html>
+  <head>
+    <meta name="go-import" content="dario.lol/gotils git https://github.com/dajooo/gotils" />
+    <meta name="go-source" content="dario.lol/gotils https://github.com/dajooo/gotils https://github.com/dajooo/gotils/tree/main{/dir} https://github.com/dajooo/gotils/blob/main{/dir}/{file}#L{line}">
+  </head>
+</html>`
   }
   return sendRedirect(event, 'https://pkg.go.dev/dario.lol/gotils')
 })
